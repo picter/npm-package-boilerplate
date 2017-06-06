45a5d379b811f9950d8298203458158e07792487
Gabriel Neutzling <gabrielneutzling@gmail.com>
2017-06-06 19:16:56 -0300

    Add localized messages to be used on example component.

---
+++ src/messages.js
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  hi: {
+    id: 'Example.title.hi',
+    defaultMessage: 'Hi {name}!',
+  },
+  welcome: {
+    id: 'Example.message.welcome',
+    defaultMessage: 'Welcome to the initial example of {value}',
+  },
+});
