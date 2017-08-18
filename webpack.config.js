7ff3141bf40e66843d7fceb28a461d129f2a52dc
Gabriel Neutzling <gabrielneutzling@gmail.com>
2017-08-18 11:31:32 -0300

    Remove unused API_URL env var.

--- webpack.config.js
+++ webpack.config.js
@@ -44,11 +44,4 @@ module.exports = {
       }
     ],
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        API_URL: JSON.stringify(process.env.API_URL),
-      },
-    }),
-  ],
 };
